6dbbc528b78ab3ebb7c59555fec625234a060cc6
Duc Jeremy Vu <duc.jeremy.vu@ucm.agency>
2023-12-18 17:45:25 +0100

    ..

--- ucm_dagster/assets.py
+++ ucm_dagster/assets.py
@@ -19,6 +19,14 @@ def ucm_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
 
 
 
+@asset
+def update_spendesk_api_key() -> None:
+    client_id = os.getenv('SPENDESK_CLIENT_ID')
+    client_secret = os.getenv('SPENDESK_CLIENT_SECRET')
+    token = create_access_token(client_id=client_id, client_secret=client_secret)
+    patch_spendesk_api_token(token)
+
+
 
 airbyte_instance = AirbyteResource(
     host="localhost",
@@ -30,17 +38,6 @@ airbyte_instance = AirbyteResource(
 
 # Use the airbyte_instance resource we defined in Step 1
 airbyte_assets = load_assets_from_airbyte_instance(airbyte_instance)
-
-
-
-@asset
-def update_spendesk_api_key() -> None:
-    client_id = os.getenv('SPENDESK_CLIENT_ID')
-    client_secret = os.getenv('SPENDESK_CLIENT_SECRET')
-    token = create_access_token(client_id=client_id, client_secret=client_secret)
-    patch_spendesk_api_token(token)
-
-
 
 
 
